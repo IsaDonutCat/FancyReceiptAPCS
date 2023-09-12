ca10fad21fcf180c667a7e384ced2f031f6e9113
Isabella Wang <wangisa26@7hills.org>
2023-09-12 13:09:32 -0400

    in class start

--- FancyReceipt.java
+++ FancyReceipt.java
@@ -5,6 +5,18 @@ public class FancyReceipt {
 
     public String item1, item2, item3, item4, item5, item6;
     public double price1, price2, price3, price4, price5, price6;
+    public boolean done = false;
+    public boolean fin = false;
+
+    public boolean nextCustom()
+    {
+        greet();
+        while (!done){
+        scanItems();
+        }
+        checkout();
+        return fin;
+    }
 
     public void greet()
     {
@@ -14,10 +26,11 @@ public class FancyReceipt {
     public void scanItems()
     {
         System.out.println("Scan the next item");
+        done = true;
     }
 
     public void checkout()
     {
-        
+        System.out.println("Pay me");
     }
 }
